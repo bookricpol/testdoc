0fe2fbd6ee57da1a37987b438967345fe7820c46
bookricpol <r.polignieri@gmail.com>
2020-04-18 22:11:20 +0200

    rtd quirkness

--- docs/conf.py
+++ docs/conf.py
@@ -13,7 +13,7 @@
 import os
 import sys
 sys.path.insert(0, os.path.abspath('..\\src\\testdoc'))
-
+master_doc = 'index'
 
 # -- Project information -----------------------------------------------------
 
